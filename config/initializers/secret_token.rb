8e78743026a321db58ca9d8ffee7a863fcec4add
Ian OCONNOR <ipwoconnor@gmail.com>
2013-11-13 15:02:26 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HolisticEntomologyTake2::Application.config.secret_key_base = 'bbb2638cb5cbdc5a45a2921193df4568bea04a2f0399adeea79c1c65796795863da242f4724a095b110aefdf42ae600c1e81c3d965a988cf0e380f462a80a23f'
